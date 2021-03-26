686b9ec440a723b63cf321430ae31f792320dbe6
Alyx Dow <dowalyx@gmail.com>
2021-03-26 14:18:27 +0200

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridAnketa = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridAnketa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridAnketa
+            // 
+            this.dataGridAnketa.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridAnketa.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridAnketa.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridAnketa.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.dataGridAnketa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridAnketa.Location = new System.Drawing.Point(21, 12);
+            this.dataGridAnketa.Name = "dataGridAnketa";
+            this.dataGridAnketa.RowTemplate.Height = 25;
+            this.dataGridAnketa.Size = new System.Drawing.Size(767, 167);
+            this.dataGridAnketa.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridAnketa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridAnketa)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridAnketa;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        string conString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\alexdow\source\repos\WinFormsApp1\WinFormsApp1\bd.mdf;Integrated Security=True;Connect Timeout=30";
+
+        DataTable dataTable = new DataTable();
+
+        public void GetTable(string query)
+        {
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(conString))
+                {
+                    con.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+                    DataSet dataSet = new DataSet();
+                    adapter.Fill(dataSet);
+                    dataTable = dataSet.Tables[0];
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                throw;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string query = @"select * from Anketa";
+            GetTable(query);
+            dataGridAnketa.DataSource = dataTable;
+            dataGridAnketa.Columns[0].HeaderText = "№";
+            dataGridAnketa.Columns[1].HeaderText = "ФИО";
+            dataGridAnketa.Columns[2].HeaderText = "Курс";
+            dataGridAnketa.Columns[3].HeaderText = "Cпециальность";
+            dataGridAnketa.Columns[4].HeaderText = "Группа";
+        }
+
+
+    }
+}
